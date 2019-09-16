3bf942fd60eea7a83f0e133a45f6fbb90b303091
peterhangg <peterhanggg@gmail.com>
2019-09-16 19:27:54 +0000

    head

---
+++ head.js
@@ -0,0 +1,18 @@
+// FUNCTION IMPLEMENTATION
+
+const head = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    return arr[0];  
+  }
+}
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// // TEST CODE
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
