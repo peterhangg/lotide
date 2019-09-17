e9409697a48f3fb47e0f829c562de99af64731d7
peterhangg <peterhanggg@gmail.com>
2019-09-17 21:22:44 +0000

    lotide

---
+++ eqArrays.js
@@ -0,0 +1,26 @@
+const eqArrays = (arr1, arr2)=> {
+  // if(arr1.length !== arr2.length){
+  //   return false;
+  // }
+  for(let i = 0; i < arr1.length; i++){
+    if (arr1[i] !== arr2[i]){
+      return false;
+    }
+  }
+  return true;
+}
+console.log(eqArrays([1, 2, 3], [1, 2, 3]));
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]));
+
+// // FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// // // TEST CODE
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+
