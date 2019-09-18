a2f89ee91dcd3a93124a08cbca85bd6e5d58d1a6
peterhangg <peterhanggg@gmail.com>
2019-09-18 17:45:06 +0000

    letterPosition

---
+++ letterPositions.js
@@ -0,0 +1,39 @@
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] === arr2[i]) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const letterPositions = str => {
+  let result = {};
+  str = str.split("");
+  for (let i = 0; i < str.length; i++) {
+    let letter = str[i];
+    if (letter === " ") {
+      continue;
+    } else if (!result[letter]) {
+      result[letter] = [i];
+    } else {
+      result[letter].push(i);
+    }
+  }
+  return result;
+};
+
+// // TEST CODE
+// console.log(letterPosition("lighthouse in the house"));
+assertArraysEqual(letterPositions("hello").e, [1]);
