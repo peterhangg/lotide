d43e054e6e84bbab6561b12a6ac04d1b8c4c64f9
peterhangg <peterhanggg@gmail.com>
2019-09-23 18:52:25 +0000

    tail refactor

--- test/headTest.js
+++ test/headTest.js
@@ -1,6 +1,6 @@
-// // TEST CODE
 const assertEqual = require("../assertEqual");
 const head = require("../head");
 
+// TEST CODE
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
