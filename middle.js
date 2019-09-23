27271e5003020f65f0d9e791b0c60e01d004bf36
peterhangg <peterhanggg@gmail.com>
2019-09-23 20:33:56 +0000

    headTest

--- middle.js
+++ middle.js
@@ -1,22 +1,25 @@
-const eqArrays = (arr1, arr2) => {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] === arr2[i]) {
-      return true;
-    }
-  }
-  return false;
-};
+const eqArrays = require("./eqArrays");
+const assertArraysEqual = require("./assertArraysEqual")
 
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+// const eqArrays = (arr1, arr2) => {
+//   if (arr1.length !== arr2.length) {
+//     return false;
+//   }
+//   for (let i = 0; i < arr1.length; i++) {
+//     if (arr1[i] === arr2[i]) {
+//       return true;
+//     }
+//   }
+//   return false;
+// };
+
+// const assertArraysEqual = function(actual, expected) {
+//   if (eqArrays(actual, expected)) {
+//     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+//   } else {
+//     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+//   }
+// };
 
 const middle = function(array) {
   const mid1 = array[Math.floor((array.length / 2)) - 1];
@@ -33,8 +36,4 @@ const middle = function(array) {
   return middle;
 };
 
-// TEST CODE
-assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]),[3]); // => [3]
-// console.log(middle([0,1,2,3,4,5]))
-assertArraysEqual(middle(([1, 2, 3, 4, 5, 6])), [3, 4]);
+module.exports = middle
